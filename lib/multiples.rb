c0bc119cd44cdaf2f0b76cb2481dd7b4d49ee15a
PoloPro <wesleybdavis@gmail.com>
2016-06-22 23:16:25 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,26 @@
-# Enter your procedural solution here!
+def multiple_of_3?(number)
+  number % 3 == 0
+end
+
+def multiple_of_5?(number)
+  number % 5 == 0
+end
+
+def collect_multiples(limit)
+  i = 1
+  multiples = []
+  while i < limit
+    if multiple_of_3?(i)
+      multiples << i
+    elsif multiple_of_5?(i)
+      multiples << i
+    end
+    i += 1
+  end
+  multiples
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,35 @@
-# Enter your object-oriented solution here!
+
+class Multiples
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def multiple_of_3?(number)
+    number % 3 == 0
+  end
+
+  def multiple_of_5?(number)
+    number % 5 == 0
+  end
+
+  def collect_multiples
+    i = 1
+    multiples = []
+    while i < input
+      if multiple_of_3?(i)
+        multiples << i
+      elsif multiple_of_5?(i)
+        multiples << i
+      end
+      i += 1
+    end
+    multiples
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+
+end
